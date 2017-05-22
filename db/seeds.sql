57baead01ad6228e86437b06fd21eaf6799cf22a
Nicholas Xanttopoulos <nxanttopoulos@gmail.com>
2017-05-22 15:21:44 -0400

    filed added

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name) VALUES ('Angus Burger');
+INSERT INTO burgers (burger_name) VALUES ('Cheeseburger');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Chili burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Veggie burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Teriyaki burger', true);
+INSERT INTO burgers (burger_name) VALUES ('Slider');
